1b3f67a3de7ecad8117e1efd392b604c29eaca0b
Manuel Razuri <1412938@utp.edu.pe>
2022-05-25 02:35:49 -0500

    colocar links en home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./home.scss";
 import gokufront from "../../IMG/gokufront.svg";
-import facebookData from "../../IMG/facebook.svg";
-import linkenidData from "../../IMG/linkenid.svg";
-import instadData from "../../IMG/instagram.svg";
-
+// import facebookData from "../../IMG/facebook.svg";
+// import linkenidData from "../../IMG/linkenid.svg";
+// import instadData from "../../IMG/instagram.svg";
+// import githubData from "../../IMG/github.svg";
+// import wtspData from "../../IMG/whatsapp.svg";
+import facebookData from "../../IMG/icon-facebook.png";
+import linkenidData from "../../IMG/icon-linkenid.png";
+import instadData from "../../IMG/icon-instagram.png";
+import githubData from "../../IMG/icon-github.png";
+import wtspData from "../../IMG/icon-whatsapp.png";
 export const Home = () => {
   return (
     <>
@@ -21,30 +27,69 @@ export const Home = () => {
           <div className="home__container-image" data-aos="flip-left">
             <img src={gokufront} alt="gokufrontend" />
           </div>
-          <div className="home__container-links"></div>
-          <div>
-            <img
-              src={facebookData}
-              alt="Iconofacebook"
-              width="50px"
-              height="50px"
-            />
-          </div>
-          <div>
-            <img
-              src={linkenidData}
-              alt="IconolinkenId"
-              width="50px"
-              height="50px"
-            />
-          </div>
-          <div>
-            <img
-              src={instadData}
-              alt="Iconoinstagram"
-              width="50px"
-              height="50px"
-            />
+          <div className="home__container-links">
+            <div>
+              <a
+                href="https://www.facebook.com/ManuelRazuriSeminario/"
+                target="_blank"
+              >
+                <img
+                  src={facebookData}
+                  alt="Iconofacebook"
+                  width="30px"
+                  height="30px"
+                />
+              </a>
+            </div>
+            <div>
+              <a
+                href="https://www.linkedin.com/in/razurimanuel/"
+                target="_blank"
+              >
+                <img
+                  src={linkenidData}
+                  alt="IconolinkenId"
+                  width="30px"
+                  height="30px"
+                />
+              </a>
+            </div>
+            <div>
+              <a
+                href="https://www.instagram.com/manuelrazuriseminario/?hl=es-la"
+                target="_blank"
+              >
+                <img
+                  src={instadData}
+                  alt="Iconoinstagram"
+                  width="30px"
+                  height="30px"
+                />
+              </a>
+            </div>
+            <div>
+              <a href="https://github.com/Manuelrazuri" target="_blank">
+                <img
+                  src={githubData}
+                  alt="Iconogithub"
+                  width="30px"
+                  height="30px"
+                />
+              </a>
+            </div>
+            <div>
+              <a
+                href="https://api.whatsapp.com/send?phone=+51 963002441&text=Hola,%20quiero%20contactarme%20contigo%20para..."
+                target="_blank"
+              >
+                <img
+                  src={wtspData}
+                  alt="Iconogithub"
+                  width="30px"
+                  height="30px"
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
